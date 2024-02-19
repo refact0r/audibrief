787cd03072bfc7c8441c1c023b560ecec684975e
refact0r <yifanw06@gmail.com>
2024-02-19 00:39:23 -0800

    test running individual requests

--- feed3.py
+++ feed3.py
@@ -36,53 +36,47 @@ for entry in feed.entries:
 
 client = openai.OpenAI(api_key="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")
 
-prompt = (
-    "Separately summarize these 5 news articles into detailed, 1 minute summaries: "
-)
+for article in articles:
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "Summarize this news article with detail: "
+                + article["content"],
+            },
+        ],
+    )
 
-for i in range(len(articles)):
-    prompt += str(i + 1) + ". " + articles[i]["content"] + "\n"
+    print(completion)
+    print()
 
-print(prompt)
+    summary = completion.choices[0].message.content
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": prompt,
+    print(summary)
+    print()
+
+    url = "https://api.elevenlabs.io/v1/text-to-speech/Xb7hH8MSUJpSbSDYk0k2"
+
+    payload = {
+        "model_id": "eleven_turbo_v2",
+        "text": summary,
+        "voice_settings": {
+            "similarity_boost": 0.5,
+            "stability": 0.75,
+            "style": 0,
+            "use_speaker_boost": True,
         },
-    ],
-)
+    }
+    headers = {
+        "xi-api-key": "302cd538e666de491d77b1cc388c8a20",
+        "Content-Type": "application/json",
+    }
 
-print(completion)
-print()
+    eleven_response = requests.request("POST", url, json=payload, headers=headers)
 
-summary = completion.choices[0].message.content
-
-print(summary)
-
-url = "https://api.elevenlabs.io/v1/text-to-speech/Xb7hH8MSUJpSbSDYk0k2"
-
-payload = {
-    "model_id": "eleven_turbo_v2",
-    "text": summary,
-    "voice_settings": {
-        "similarity_boost": 0.5,
-        "stability": 0.75,
-        "style": 0,
-        "use_speaker_boost": True,
-    },
-}
-headers = {
-    "xi-api-key": "302cd538e666de491d77b1cc388c8a20",
-    "Content-Type": "application/json",
-}
-
-eleven_response = requests.request("POST", url, json=payload, headers=headers)
-
-CHUNK_SIZE = 1024
-with open("output.mp3", "wb") as f:
-    for chunk in eleven_response.iter_content(chunk_size=CHUNK_SIZE):
-        if chunk:
-            f.write(chunk)
+    CHUNK_SIZE = 1024
+    with open(f"{article['title'][0:10]}.mp3", "wb") as f:
+        for chunk in eleven_response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
