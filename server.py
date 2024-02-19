1f85302914f75d3eeecd1bd2276411dd2e94c0c2
refact0r <yifanw06@gmail.com>
2024-02-19 01:35:21 -0800

    configure basic quart server

--- server.py
+++ server.py
@@ -1,8 +1,47 @@
-from flask import Flask, render_template
+from datetime import datetime
+from quart import Quart
+from quart import request
+from cachetools import cached, TTLCache
+import requests
+import feedparser
+import openai
+from bs4 import BeautifulSoup
+from pathlib import Path
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
-@app.route("/")
-def root():
-    return render_template("index.html")
+@app.post("/")
+async def root():
+    data = await request.get_json()
+    return getArticles()
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=600))
+def getArticles():
+    url = "https://news.google.com/rss"
+    feed = feedparser.parse(url)
+    done = 0
+    articles = []
+    for entry in feed.entries:
+        print(entry.link)
+        response = requests.get(entry.link)
+        print(response.url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        content = ""
+        for p in soup("h1") + soup("p"):
+            content += p.get_text(strip=True) + "\n"
+        if len(content) > 200:
+            done += 1
+        else:
+            continue
+        articles.append(
+            {"title": entry.title, "content": content, "link": response.url}
+        )
+        if done == 5:
+            break
+
+    return articles
+
+
+app.run()
