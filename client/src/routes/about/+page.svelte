e940833021362fc12c4f84bcbd44c0d40f4c424b
refact0r <yifanw06@gmail.com>
2024-02-21 15:26:24 -0800

    fix about

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -1,10 +1,13 @@
-<p>Made by refact0r, Harry, Jay, and Gani for the Spring 2024 EmP Hackfest.</p>
+<p>
+	Made by refact0r, Harry, Jay, and Gani for the Spring 2024 EmP Hackfest.<br /><br />
 
-<a href="https://github.com/refact0r/audibrief">https://github.com/refact0r/audibrief</a>
+	<a href="https://github.com/refact0r/audibrief">https://github.com/refact0r/audibrief</a>
+</p>
 
 <style>
 	p {
 		margin: 2rem 0;
+		font-size: 1.2rem;
 		background-color: var(--bg-1);
 		border-radius: 2rem;
 		padding: 1.5rem;
