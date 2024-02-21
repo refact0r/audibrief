2079002629ef0417aa13cb48435642495bc34f5a
refact0r <yifanw06@gmail.com>
2024-02-21 15:15:16 -0800

    update

--- client/src/routes/about/+page.svelte
+++ client/src/routes/about/+page.svelte
@@ -1,4 +1,6 @@
-<p>Made by Yifan, Harry, Jay, and Gani for the Spring 2024 EmP Hackfest.</p>
+<p>Made by refact0r, Harry, Jay, and Gani for the Spring 2024 EmP Hackfest.</p>
+
+<a href="https://github.com/refact0r/audibrief">https://github.com/refact0r/audibrief</a>
 
 <style>
 	p {
